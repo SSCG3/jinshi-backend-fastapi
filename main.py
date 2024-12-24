5c5fc10a873d5f3b1338f070f00b2a3b07887218
root <root@localhost.localdomain>
2024-12-24 10:12:23 +0800

    add welcome message

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 load_dotenv()  # 这会加载 .env 文件中的环境变量
 from app import app
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=3030)
